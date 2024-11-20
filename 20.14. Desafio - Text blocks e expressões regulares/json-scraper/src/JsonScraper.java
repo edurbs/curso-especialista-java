7e15b98a754c72ad3e397570f989b308ac2fb98e
Eduardo Soares <edurbs@gmail.com>
2024-11-20 18:11:12 -0300

    add desafio 20.14

--- 20.14. Desafio - Text blocks e expressões regulares/json-scraper/src/JsonScraper.java
+++ 20.14. Desafio - Text blocks e expressões regulares/json-scraper/src/JsonScraper.java
@@ -1,19 +1,31 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class JsonScraper {
 
     public static void main(String[] args) {
-        String json = "{\n" +
-                "    \"id\": 1,\n" +
-                "    \"nome\": \"João da Silva\"\n" +
-                "},\n" +
-                "{\n" +
-                "    \"id\": 2,\n" +
-                "    \"nome\":\"Maria Abadia\"\n" +
-                "},\n" +
-                "{\n" +
-                "    \"id\": 3,\n" +
-                "    \"nome\":\n" +
-                "        \"Sebastião Carvalho\"\n" +
-                "}";
+        String json = """
+                {
+                    "id": 1,
+                    "nome": "João da Silva"
+                },
+                {
+                    "id": 2,
+                    "nome":"Maria Abadia"
+                },
+                {
+                    "id": 3,
+                    "nome":
+                        "Sebastião Carvalho"
+                }""";
+
+        String group = "names";
+        String regex = "\"nome\":\\s*\"(?<"+group+">.*?)\"";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(json);
+        while(matcher.find()){
+            System.out.println(matcher.group(group));
+        }
     }
 
 }
