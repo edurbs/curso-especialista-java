4c94936d8ec5dee992521e999a2e77376f2534fe
Eduardo Soares <edurbs@gmail.com>
2024-12-04 18:16:50 -0300

    add desafio 32.14

--- 32.14. Desafio - Logback e SLF4J/fragmentador-arquivos/src/main/java/com/algaworks/arquivos/Principal.java
+++ 32.14. Desafio - Logback e SLF4J/fragmentador-arquivos/src/main/java/com/algaworks/arquivos/Principal.java
@@ -1,21 +1,26 @@
 package com.algaworks.arquivos;
 
-import com.algaworks.arquivos.fragmentador.FragmentadorDeArquivo;
-
 import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.algaworks.arquivos.fragmentador.FragmentadorDeArquivo;
 
 public class Principal {
 
+    private static final Logger logger = LoggerFactory.getLogger(Principal.class);
+
     public static void main(String[] args) {
         try {
-            System.out.println("INFO: Iniciando programa de fragmentação...");
+            logger.info("INFO: Iniciando programa de fragmentação...");
             var fragmentador = new FragmentadorDeArquivo(
                     Path.of("fotos/picanha.jpg"),
                     1024 * 50);
             fragmentador.fragmentar();
-            System.out.println("INFO: Fragmentação concluída com sucesso!");
+            logger.info("INFO: Fragmentação concluída com sucesso!");
         } catch (Exception e) {
-            System.out.println("Erro fragmentando arquivo");
+            logger.error("Erro fragmentando arquivo");
             e.printStackTrace();
         }
     }
